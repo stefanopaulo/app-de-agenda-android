b9431085c9a6c876ebe1caa6372fb55e97d11fd7
stefanotrab <stefano.souza@hapvida.com.br>
2021-03-22 22:18:29 -0300

    inserindo icone e app bar

--- app/src/main/java/com/app/agenda/MainActivity.java
+++ app/src/main/java/com/app/agenda/MainActivity.java
@@ -7,16 +7,18 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de alunos");
 
         List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José", "Maria", "Ana"));
 

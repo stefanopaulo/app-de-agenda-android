cf623cf5e07b998a806ed2b13a58f6736421795e
Stefano Paulo <stefanopaulo47@gmail.com>
2021-03-27 21:42:57 -0300

    adicionando o dialog

--- app/src/main/java/com/app/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/app/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,5 +1,7 @@
 package com.app.agenda.ui.activity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -33,6 +35,14 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setTitle(TITULO_APPBAR);
         configuraFabNovoAluno();
         configuraLista();
+
+        new AlertDialog
+                .Builder(this)
+        .setTitle("Removendo aluno")
+        .setMessage("Tem certeza que quer remover o aluno?")
+        .setPositiveButton("Sim", null)
+        .setNegativeButton("Não", null)
+        .show();
     }
 
     @Override
